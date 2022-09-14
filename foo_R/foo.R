b36e2560356b92a479e85e098a70de6bb85c6bbe
Bastien Winant <babw1@student.london.ac.uk>
2022-09-14 10:32:30 +0200

    move the computation of sphere volume to a separate function

--- foo_R/foo.R
+++ foo_R/foo.R
@@ -1,6 +1,6 @@
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-  3/4*pi*r^2
+volume <- function(r) {
+  return (4/3 * pi * r^3)
 }
 
 # Function to compute the volumes of the spheres with radius r, r^2 and r^3
@@ -9,8 +9,7 @@ volume_vector <- function(r) {
   
   for (i in 1:3) {
     radius = r^i
-    volume = 4/3 * pi * radius^3
-    print(volume)
+    volume = volume(radius)
     sphere_volumes[i] = volume
   }
   
