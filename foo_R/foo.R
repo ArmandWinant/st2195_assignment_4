ce3bf8126a6a25e6e44977b99bdc80345271e81a
Bastien Winant <babw1@student.london.ac.uk>
2022-09-27 22:03:28 +0200

    use defensive programming to protect against non-numerical inputs

--- foo_R/foo.R
+++ foo_R/foo.R
@@ -7,18 +7,21 @@ volume <- function(r) {
 volume_vector <- function(r) {
   sphere_volumes <- numeric()
   
-  for (i in 1:3) {
-    radius = r^i
-    volume = volume(radius)
-    sphere_volumes[i] = volume
+  if (is.numeric(r) && (r >= 0)) {
+    for (i in 1:3) {
+      radius = r^i
+      volume = volume(radius)
+      sphere_volumes[i] = volume
+    }
   }
   
   return (sphere_volumes)
 }
 
 # Radius
-r <- 2
+r <- "1"
 
 # Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
 volumes <- volume_vector(r)
 print(volumes)
+
